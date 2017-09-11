7562d84d9ecbbc91740f92e376317c26eb169820
fengli738 <fli19@hawk.iit.edu>
2017-09-11 11:37:27 -0500

    week3
    Week3 Homework

---
+++ Assignment/week3/selectors.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+$(".relevant p:nth-child(1)").css("background-color", "blue");
+$(".relevant p:nth-child(2)").css("background-color", "red");
+$(".relevant p:nth-child(3)").css("background-color", "yellow");
+$(".relevant p:nth-child(4)").css("background-color", "green");
+$(".relevant p:nth-child(5)").css("background-color", "purple");
+$(".relevant p:nth-child(6)").css("background-color", "grey");
+$(".relevant p:nth-child(7)").css("background-color", "orange");
+           });
+
+
+
+
+
+function fizzbuzz(){
+	var number;
+	number = prompt ("please enter a number");
+}
